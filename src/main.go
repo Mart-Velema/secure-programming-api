792277bb0a6f02391788e7ddfff4ff2f67feb302
Mart-Velema <mart.velema@student.nhlstenden.com>
2026-05-06 19:04:51 +0200

    Added Go boilerpalte

---
+++ src/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func HelloWorld(c *gin.Context) {
+	c.String(200, "%s", "Hello, world!")
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s\n", err)
+	}
+	fmt.Printf("Running locally on localhost:%s", os.Getenv("PORT"))
+
+	router := gin.Default()
+
+	router.GET("/api/v1", HelloWorld)
+
+	err = router.Run(fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT")))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
