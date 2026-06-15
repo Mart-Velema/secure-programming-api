420d9696e14b0339076f50bcce8d6c07b4a1258e
Lars Kuijer <larsk1201@gmail.com>
2026-06-15 20:15:26 +0200

    return whole inventory payload

--- steam-bot/src/services/inventoryService.js
+++ steam-bot/src/services/inventoryService.js
@@ -8,29 +8,7 @@ function getBotInventory(appId = 440, contextId = 2) {
         return;
       }
 
-      resolve(
-        inventory.map((item) => ({
-          assetId: item.assetid,
-          classId: item.classid,
-          instanceId: item.instanceid,
-          appId: item.appid,
-          contextId: item.contextid,
-
-          marketHashName: item.market_hash_name,
-          name: item.name,
-          type: item.type,
-          iconUrl: item.icon_url,
-          iconUrlLarge: item.icon_url_large,
-
-          tradable: item.tradable,
-          marketable: item.marketable,
-
-          descriptions: item.descriptions,
-          actions: item.actions,
-          marketActions: item.market_actions,
-          tags: item.tags,
-        }))
-      );
+      resolve(inventory);
     });
   });
 }
